a6feae6d3477b63718b0171ac22129df1e0145bd
Saul Moreno Abril <93sauu@gmail.com>
2020-10-24 18:24:09 +0200

    Config api middleware

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,23 +1,27 @@
-import {configureStore, combineReducers, getDefaultMiddleware} from '@reduxjs/toolkit';
-import {persistStore, persistReducer} from 'redux-persist'
+import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { createMiddleware as createApiMiddeleware } from 'redux-api-middleware';
+import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import stocksReducer from './features/stocksSlice';
+import stocksReducer from './features/stocksSlice'
 
 const reducers = combineReducers({
-    stocks: stocksReducer,
+  stocks: stocksReducer,
 })
 
+const apiMiddleware = createApiMiddeleware()
+const middleware = [...getDefaultMiddleware({
+  serializableCheck: false,
+}), apiMiddleware]
+
 const persistConfig = {
-    key: 'root',
-    storage,
+  key: 'root',
+  storage,
 }
 
 const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: getDefaultMiddleware({
-        serializableCheck: false,
-    })
+  reducer: persistedReducer,
+  middleware: middleware
 })
 export const persistor = persistStore(store)
