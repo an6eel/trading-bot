5600bac4cd0380d8eb91a05b0b9ee7ac9bf1cb4d
Angel Garcia <agarciamal1997@gmail.com>
2020-10-29 01:07:15 +0100

    Added traefik proxy

--- stock_predictor/main.py
+++ stock_predictor/main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from db.helpers import close_mongo, connect_to_mongo
 from routers.stocks import router as stock_router
 from routers.models import router as models_router
-from core.config import TRAIN_TYPE
 
 app = FastAPI()
-base_router = APIRouter()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,9 +17,8 @@ app.add_middleware(
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo)
 
-base_router.include_router(stock_router, prefix="/stocks", tags=["stocks"])
-base_router.include_router(models_router, prefix="/ml", tags=["models"])
+app.include_router(stock_router, prefix="/stocks", tags=["stocks"])
+app.include_router(models_router, prefix="/ml", tags=["models"])
 
-app.include_router(base_router, prefix="/{}".format(TRAIN_TYPE))
 
 
